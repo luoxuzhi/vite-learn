94e024fd3805b4aeba82bcdd2b35fe7ec20142c9
luoxuzhi <herolxz@163.com>
2023-08-12 21:49:34 +0800

    feat:init

---
+++ src/about.vue
@@ -0,0 +1,33 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const props=defineProps({
+  name:String
+})
+console.log(route.params)
+</script>
+
+<template>
+  <div>
+    jkljkjljklj
+    <div>
+      {{name}} ???? {{route.params.name}}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../App.vue'
+import About from '../about.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about/:name', component: About,props:true },
+]
+
+const router = createRouter({
+  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+  history: createWebHashHistory(),
+  routes, // `routes: routes` 的缩写
+})
+
+export { router }
